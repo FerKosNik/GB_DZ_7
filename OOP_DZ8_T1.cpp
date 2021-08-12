96c02fbc392a31555c49995b60c4de83e6560bdb
FerKosNik <ferafontov83@mail.ru>
2021-08-12 23:45:15 +0300

    Create OOP_DZ8_T1.cpp

---
+++ OOP_DZ8_T1.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+using namespace std;
+
+// Отдельная функция деления
+double div(double a, double b)
+{
+    // Если пользователь ввёл частное = 0, то выбрасываем исключение
+    if (b == 0.0)
+        throw "Can not division to zero"; // выбрасывается исключение типа const char*
+
+    return a/b;
+}
+
+int main()
+{
+    cout << "Enter a divisible: ";
+    double a;
+    cin >> a;
+    cout << "Enter a quotient: ";
+    double b;
+    cin >> b;
+
+    try // ищем исключения, которые выбрасываются в блоке try, и отправляем их для обработки в блок(и) catch
+    {
+        double d = div(a, b);
+        cout << "The division " << a << " to " << b << " is " << d << '\n';
+    }
+    catch (const char* exception) // обработка исключений типа const char*
+    {
+        cerr << "Error: " << exception << endl;
+    }
+
+    return 0;
+}
